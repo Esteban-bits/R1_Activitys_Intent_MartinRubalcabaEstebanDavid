ab015d4a66ab46dfc116e7ebe2094ec87875231b
Esteban-bits <juanesito84@gmail.com>
2025-09-04 23:43:42 -0600

    hola

--- app/src/main/java/com/example/actividadapp/Activity2.kt
+++ app/src/main/java/com/example/actividadapp/Activity2.kt
@@ -1,6 +1,7 @@
 package com.example.actividadapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,9 +12,16 @@ class Activity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_2)
+
+        val textViewDatoRecibido = findViewById<TextView>(R.id.textViewDatoRecibido)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+            val datoRecibido = intent.getStringExtra("dato")
+            textViewDatoRecibido.text = "Nombre: $datoRecibido"
+
             insets
         }
     }
--- app/src/main/java/com/example/actividadapp/MainActivity.kt
+++ app/src/main/java/com/example/actividadapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,19 +14,23 @@ class MainActivity : AppCompatActivity() {
 
     private val TAG = "Cycle APP"
     lateinit var myButton: Button
+    lateinit var myEdit: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         Log.i(TAG, "Go onCreate!")
 
-        myButton = findViewById<Button>(R.id.btn_intent)
+        myButton = findViewById(R.id.btn_intent)
+        myEdit = findViewById(R.id.editText)
     }
 
     override fun onStart() {
@@ -33,34 +38,16 @@ class MainActivity : AppCompatActivity() {
         Log.i(TAG, "Go onStart!")
 
         myButton.setOnClickListener {
-            //Intent de tipo Explicito
-            val intent = Intent(this, Activity2::class.java)
-            startActivity(intent)
+            val dato = myEdit.text.toString().trim()
+
+            if (dato.isEmpty()) {
+                myEdit.error = "Debes Ingresar tu nombre."
+            } else {
+                val intent = Intent(this, Activity2::class.java).apply {
+                    putExtra("dato", dato)
+                }
+                startActivity(intent)
+            }
         }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i(TAG, "Go onResume!")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i(TAG, "Go onPause!")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i(TAG, "Go onStop!")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.i(TAG, "Go onRestart!")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i(TAG, "Go onDestroy!")
     }
 }
